d956dc9af50177e881ded2d03a9ca26a5fc43553
Shangyangyang <602390269@qq.com>
2018-03-28 14:05:04 +0800

    初始导入

---
+++ src/main/java/cn/ainannan/HuozheApplication.java
@@ -0,0 +1,12 @@
+package cn.ainannan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuozheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuozheApplication.class, args);
+	}
+}
